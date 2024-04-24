6d319004afad378e1bc082834ae79da22bd7b57b
unwontedsponn <benpaulspooner@gmail.com>
2024-04-24 14:36:06 +0100

    CHanged Login screen to coming soon banner

--- app/components/mainWebsitePages/Login.js
+++ app/components/mainWebsitePages/Login.js
@@ -20,8 +20,12 @@ export default function Login({onLoginSuccess}) {
           /*--------------------------------------------------------------------------------------------------------------------*/
         }
         <div className="h-screen overflow-hidden flex justify-center items-center">
-          <button className="font-gopher-mono items-center color-dark border-y-3 border-thick-border-gray py-1 px-2 hover:bg-gray-200" onClick={handleLogin}>Login</button>
-          {/* <button className="font-gopher-mono items-center color-dark border-3 border-thick-border-gray py-1 px-2 hover:bg-gray-200 mb-4" onClick={randomizeCombination}>Login</button> */}
+          
+          {/* Login Button */}
+          {/* <button className="font-gopher-mono items-center color-dark border-y-3 border-thick-border-gray py-1 px-2 hover:bg-gray-200" onClick={handleLogin}>Login</button> */}
+
+          {/* COMING SOON banner */}
+          <p className="font-gopher-mono items-center color-dark border-y-3 border-thick-border-gray py-1 px-2">COMING SOON</p>
         </div>
 
     </section>
