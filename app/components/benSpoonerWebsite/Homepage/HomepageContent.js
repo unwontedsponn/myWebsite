21d3703bfec11ef1e48da94973565c91a3153d7e
unwontedsponn <benpaulspooner@gmail.com>
2024-05-01 18:03:08 +0100

    Escaped text in Homepage content and mybook content

--- app/components/benSpoonerWebsite/Homepage/HomepageContent.js
+++ app/components/benSpoonerWebsite/Homepage/HomepageContent.js
@@ -2,9 +2,11 @@ export const smallText = 'I like to get things done';
 
 export const longText = (
   <p>
-    I thrive on bringing ideas to life. From creating books and games to websites and musical pieces, I'm fuelled by a passion for creative endeavours, and <span className="italic">getting things done</span>
+    {`I thrive on bringing ideas to life. From creating books and games to websites and musical pieces, I'm fuelled by a passion for creative endeavours, and `}
+    <span className="italic">getting things done</span>
   </p>
 );
+
 
 export const imageAttributes = {
   src: '/images/homepage-pic.jpeg',
