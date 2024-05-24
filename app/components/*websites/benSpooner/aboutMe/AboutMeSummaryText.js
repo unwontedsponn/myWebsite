d708b0746a09a931eedad05f0685fbb78ed9e145
unwontedsponn <benpaulspooner@gmail.com>
2024-05-24 10:14:58 +0100

    Update text in aboutMe

--- app/components/*websites/benSpooner/aboutMe/AboutMeSummaryText.js
+++ app/components/*websites/benSpooner/aboutMe/AboutMeSummaryText.js
@@ -9,33 +9,30 @@ export default function AboutMeSummaryText({ }) {
   
   const textOptions = [
     <>
-      <p>Hello there! I&apos;m <span className="font-gopher-mono-semi">Ben Spooner</span>, a musician, javascript enthusiast and technical support engineer at Neat. My journey into the tech world was anything but conventional. After a successful 15-year tenure as a business owner providing music and audio services, I embarked on a new adventure in web development. This leap of faith was bolstered when I secured a scholarship for a highly competitive Software Engineering & Data Science course, standing out among 20,000 applicants.</p>
+      <p>Hello there! I&apos;m <span className="font-gopher-mono-semi">Ben Spooner</span>, a musician, javascript developer and technical support engineer at Neat. My journey into the tech world was anything but conventional. After a successful 15-year career as a business owner providing music and audio services, I embarked on a new adventure in web development. This leap was bolstered when I secured a scholarship for a highly competitive Software Engineering & Data Science course, standing out among 20,000 applicants.</p>
     </>,
     <>
       <p><span className="font-gopher-mono-semi color-green ">A Glimpse into My Tech Journey:</span></p>
-      <p>My coding journey began unexpectedly while pursuing an MSc in Music and Sound. Diving into C# to integrate sound into computer games using Unity Game Engine, I discovered a newfound passion. This led me down the path of self-taught programming, culminating in the creation of this portfolio website from the ground up using Next.Js, Tailwind CSS & Vercel.</p>
+      <p>{`My coding journey began unexpectedly while pursuing an MSc in Music and Sound for Film and Games. Diving into C# to integrate sound into computer games using Unity Game Engine, I discovered a newfound passion. This led me down the path of self-taught programming, culminating in the creation of several projects using Next.Js, Tailwind CSS & Vercel (including this website).`}</p>
     </>,
     <>
       <p><span className="font-gopher-mono-semi color-green">A Brief History of My Audio Endeavors:</span></p>
-      <p>Over the last decade and a half, I&apos;ve worn many hats in the audio world - from performing with Joy Crookes and touring at the Edinburgh Festival to teaching ABRSM and examining for RSL. My composition skills were showcased in the award-winning short film When We Fell, which garnered international acclaim and several awards, including nominations from the British Animation Awards. Presently, I channel my musical creativity through independent releases as Sponn and Green and Pine.</p>
+      <p>{`Over the last decade and a half, I've worn many hats in the audio world - from performing with Joy Crookes and touring at the Edinburgh Festival to teaching ABRSM and examining for RSL. My composition skills were showcased in the award-winning short film 'When We Fell', which garnered international acclaim and several awards, including nominations from the British Animation Awards. Presently, I channel my musical creativity through independent releases as Sponn and Green and Pine.`}</p>
     </>,
     <>
       <p><span className="font-gopher-mono-semi color-green">A Little More About Me:</span></p>
       <p><span className="font-gopher-mono-semi">Age:</span> 34 years young</p>
       <p><span className="font-gopher-mono-semi">Favourite Book:</span> Hear The Wind Sing</p>
-      <p><span className="font-gopher-mono-semi">Tech Stack:</span> Next.js, Tailwind CSS & Three.js</p>
+      <p><span className="font-gopher-mono-semi">Tech Stack:</span> Next.js, Tailwind CSS & Vercel</p>
       <p><span className="font-gopher-mono-semi">Sound Stack:</span> Logic X, Sibelius & FMOD</p>
-      <p><span className="font-gopher-mono-semi">Education:</span> Music and Sound for Film and Games (MSc) - Distinction</p>
-      <p><span className="font-gopher-mono-semi">Hobbies:</span> Reading, walking, eating, learning, cooking & creating</p>
-      <p><span className="font-gopher-mono-semi">Extra Links:</span> GitHub | LinkedIn | KTMA | Blog</p>
-      <p><span className="font-gopher-mono-semi">Fun Fact:</span> I once unexpectedly jammed with the British pop icon Adam Ant at a North London gig.</p>
+      <p><span className="font-gopher-mono-semi">Hobbies:</span> Reading, learning, walking, creating, cooking, eating, Dad-ing!</p>
+      <p><span className="font-gopher-mono-semi">Extra Links:</span> 
+        <a href="https://github.com/unwontedsponn" className="hover:underline"> GitHub</a> |
+        <a href="https://www.linkedin.com/in/ben-spooner-12aab937/" className="hover:underline"> LinkedIn</a> |
+        <a href="https://sponn.itch.io/keep-the-music-alive" className="hover:underline"> KTMA</a> |
+        <a href="https://medium.com/@benspooner" className="hover:underline"> Blog</a>
+      </p>
     </>,
-    <>
-      <p><span className="font-gopher-mono-semi color-green">Current projects:</span></p>
-      <p><span className="font-gopher-mono-semi">Integrated Training Environment (ITE):</span>&quot;Realtime rhythm training via midi devices and Bela.io hardware.&quot;</p>
-      <p><span className="font-gopher-mono-semi">Music Maker Network podcast (MMN):</span> &quot;Where we chat to the people, that make music happen&quot;.</p>
-      <p>For a deeper dive into my work and adventures, check out my YouTube channel.</p>
-    </>
   ];
 
   const nextPage = () => {
